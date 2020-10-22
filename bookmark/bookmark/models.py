0f760455275c2ca7de194686752bd475bcf96699
DESKTOP-AJPNM8J\ismir <ismir1449@gmail.com>
2020-10-22 10:04:59 +0900

    4.2.5

--- bookmark/bookmark/models.py
+++ bookmark/bookmark/models.py
@@ -5,3 +5,7 @@ from django.db import models
 class Bookmark(models.Model):
     site_name = models.CharField(max_length=100)
     url = models.URLField('Site URL')
+    #4.2.5 __str__메서드 추가
+    def __str__(self):
+        #개개체를 출력할 때 나타날 값
+        return "이름 : "+self.site_name +  ", 주소 : " + self.url
